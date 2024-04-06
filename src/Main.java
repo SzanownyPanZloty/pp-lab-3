7d058ef8ef30c46cb8f87fcf798c1e628fe4506a
Jakub Zlotek <kubazlotek@gmail.com>
2024-04-06 18:10:46 +0200

    zad3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import geometry.Point;
-import geometry.Circle;
+import geometry.ColoredCircle;
 
 public class Main {
   public static void main(String[] args) throws Exception {
     Point point = new Point(3, 4);
-    Circle circle = new Circle(point, 5);
-    System.out.println(circle.calculatePerimeter());
+    ColoredCircle circle = new ColoredCircle(point, 5, "red");
+    System.out.println("Circle color: " + circle.getColor());
+    System.out.println("Circle radius: " + circle.getRadius());
+    System.out.println("Circle center: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
   }
 }
